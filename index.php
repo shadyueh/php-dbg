26ddc6e4e7b1868b6d4b9940afe7eff80ab98636
Held Grijo <held.grijo@manaus.am.gov.br>
2022-07-01 12:54:06 -0400

    refactor: cria classe para imprimir php info

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo phpinfo();
+<?php
+
+namespace App\Core;
+
+class MinhaClasse
+{
+    public static function imprimirInfo()
+    {
+        echo phpinfo();
+    }
+}
+
+MinhaClasse::imprimirInfo();
